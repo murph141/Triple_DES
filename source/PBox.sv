0966d6d3f025c050734709efdae6997537e9fcf2
Mentor Graphics <mg115@ee215lnx12.ecn.purdue.edu>
2015-04-01 18:00:35 -0400

    adding shit

---
+++ source/PBox.sv
@@ -0,0 +1,11 @@
+// $Id: $
+// File name:   PBox.sv
+// Created:     4/1/2015
+// Author:      Anthony Kang
+// Lab Section: 337-04
+// Version:     1.0  Initial Design Entry
+// Description: Permutation Box
+module PBox(
+  input wire [32:0] data,
+  output wire [32:0] p_data
+);
