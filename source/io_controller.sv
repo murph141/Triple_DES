fb7283adc24ad3510bad68304429560317d04291
sheel7 <sheeley7@gmail.com>
2015-04-01 17:58:59 -0400

    starting io controller

---
+++ source/io_controller.sv
@@ -0,0 +1,7 @@
+// $Id: $
+// File name:   io_controller.sv
+// Created:     4/1/2015
+// Author:      Isaac Sheeley
+// Lab Section: 4
+// Version:     1.0  Initial Design Entry
+// Description: io controller
