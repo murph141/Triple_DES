6695398a3a6b68e5e023fc202fa8f0b476874f3e
sheel7 <sheeley7@gmail.com>
2015-04-08 16:18:25 -0400

    starting of io controller

--- source/io_controller.sv
+++ source/io_controller.sv
@@ -5,3 +5,26 @@
 // Lab Section: 4
 // Version:     1.0  Initial Design Entry
 // Description: io controller
+module io_controller (
+	input wire sclk,
+	input wire n_rst,
+	input wire data_in,
+	output reg [63:0]data_out
+);
+	logic count_out;
+	logic count_enable;
+	logic count_hit_64;
+	logic clear_counter;
+
+	flex_counter #(6) DUT_FLEX_COUNTER
+  	(
+  		.clk          (sclk),
+  		.n_rst        (n_rst),
+  		.clear        (clear_counter),
+  		.count_enable (count_enable),
+  		.rollover_val (6'd64),
+  		.count_out    (count_out),
+  		.rollover_flag(count_hit_64)
+  	);
+
+endmodule
