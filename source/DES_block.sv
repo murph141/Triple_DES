603e934ee474cd67515dbbc6bdd40cf935568180
sheel7 <sheeley7@gmail.com>
2015-04-26 19:51:38 -0400

    gave credit

--- source/DES_block.sv
+++ source/DES_block.sv
@@ -1,7 +1,7 @@
 // $Id: $
 // File name:   DES_block.sv
 // Created:     4/26/2015
-// Author:      Isaac Sheeley
+// Author:      Isaac Sheeley & Seth Bontrager
 // Lab Section: 4
 // Version:     1.0  Initial Design Entry
 // Description: wrapper for 1 DES Block
