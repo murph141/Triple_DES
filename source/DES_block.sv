76fbfdca50cfeaf6a2eb1d9dccdf8c7f7071c4be
sheel7 <sheeley7@gmail.com>
2015-04-26 19:49:01 -0400

    DES block passes mapped tb

--- source/DES_block.sv
+++ source/DES_block.sv
@@ -12,8 +12,8 @@ module DES_block (
 	input wire encr_decr,
 	input wire[63:0] input_data_block,
 	input wire[63:0] user_key,
-	output wire[63:0] output_data_block,
-	output reg enable_next_block
+	output reg[63:0] output_data_block,
+	output wire enable_next_block
 );
 
 	//Output of KSA
@@ -41,11 +41,11 @@ module DES_block (
 	reg[31:0] left_reg_9_16;
 	reg[31:0] right_reg_9_16;
 
-	wire[31:0] next_left_reg_1_8;
-	wire[31:0] next_right_reg_1_8;
+	logic[31:0] next_left_reg_1_8;
+	logic[31:0] next_right_reg_1_8;
 
-	wire[31:0] next_left_reg_9_16;
-	wire[31:0] next_right_reg_9_16;
+	logic[31:0] next_left_reg_9_16;
+	logic[31:0] next_right_reg_9_16;
 
 
 	wire[31:0] left_out_round_1_8;
@@ -54,52 +54,68 @@ module DES_block (
 	wire[31:0] left_out_round_9_16;
 	wire[31:0] right_out_round_9_16;
 
-	wire[47:0] curr_key_1_8;
-	wire[47:0] curr_key_9_16;
+	logic[47:0] curr_key_1_8;
+	logic[47:0] curr_key_9_16;
 
 
 	reg enable_rounds_1_8;
 	reg enable_rounds_9_16;
+	reg enable_output_counter;
 
-	reg [2:0]count_1_8;
-	reg [2:0]count_9_16;
+
+	reg [3:0]count_1_8;
+	reg [3:0]count_9_16;
+	reg [3:0]count_output;
 
 	reg rollover_1_8;
 	reg rollover_9_16;
+	reg rollover_output;
 
 	reg clear_1_8;
 	reg clear_9_16;
+	reg clear_output_counter;
 
 
-
+	assign enable_next_block = enable_output_counter;
 
 
 	//First 8 rounds counter
-	flex_counter DUT_counter_1_8(
+	flex_counter #(4) DUT_counter_1_8(
 		.clk          (clk),
 		.n_rst        (nrst),
 		.clear        (clear_1_8),
 		.count_enable (enable_rounds_1_8),
-		.rollover_val (3'd8),
+		.rollover_val (4'd8),
 		.count_out    (count_1_8),
 		.rollover_flag(rollover_1_8)
 	);
 
 	//Counter for rounds 9 through 16
-	flex_counter DUT_counter_9_16(
+	flex_counter #(4) DUT_counter_9_16(
 		.clk          (clk),
 		.n_rst        (nrst),
 		.clear        (clear_9_16),
 		.count_enable (enable_rounds_9_16),
-		.rollover_val (3'd8),
+		.rollover_val (4'd8),
 		.count_out    (count_9_16),
 		.rollover_flag(rollover_9_16)
+	);
+
+	//counter enable counter
+	flex_counter #(4) DUT_counter_output(
+		.clk          (clk),
+		.n_rst        (nrst),
+		.clear        (clear_output_counter),
+		.count_enable (enable_output_counter),
+		.rollover_val (4'd8),
+		.count_out    (count_output),
+		.rollover_flag(rollover_output)
 	);
 
 	//Round registers
 	always_ff @(posedge clk, negedge nrst) 
 	begin
-		if(rst_n == 0) 
+		if(nrst == 0) 
 		begin
 			left_reg_1_8   <= 0;
 			right_reg_1_8  <= 0;
@@ -115,44 +131,89 @@ module DES_block (
 		end
 	end
 
-	//enable registers
+
+
 	always_ff @(posedge clk, negedge nrst)
 	begin
 		if(nrst == 0)
 		begin
 			enable_rounds_1_8  <= 0;
-			enable_rounds_9_16 <= 0;
-			enable_next_block  <= 0;
+			clear_1_8 <= 0;
 		end
 
-		else if(rollover_1_8 == 1)
+		else if((enable_rounds_1_8 == 0) && (enable == 1))
 		begin
-			enable_rounds_9_16 <= enable_rounds_1_8;
+			enable_rounds_1_8 <= 1;
 		end
 
-		else if(rollover_9_16)
+		else if((enable == 0) && (count_1_8 == 4'd7))
 		begin
-			enable_next_block <= enable_rounds_9_16;
+			enable_rounds_1_8 <= 0;
+			clear_1_8 <= 1;
 		end
 
 		else
 		begin
-			enable_rounds_1_8  <= enable;
-			enable_rounds_9_16 <= enable_rounds_9_16;
-			enable_next_block  <= enable_next_block;
+			clear_1_8 <= 0;
+			enable_rounds_1_8 <= enable_rounds_1_8;
 		end
 	end
 
-	//Left and right registers
 	always_ff @(posedge clk, negedge nrst)
 	begin
 		if(nrst == 0)
 		begin
+			enable_rounds_9_16 <= 0;
+			clear_9_16 <= 0;
+		end
 
+		else if((enable_rounds_9_16 == 0) && (count_1_8 == 4'd7))
+		begin
+			enable_rounds_9_16 <= 1;
+		end
+
+		else if((enable_rounds_1_8 == 0) && (count_9_16 == 4'd7))
+		begin
+			enable_rounds_9_16 <= 0;
+			clear_9_16 <= 1;
+		end
+
+		else
+		begin
+			clear_9_16 <= 0;
+			enable_rounds_9_16 <= enable_rounds_9_16;
+		end
+	end
+
+	always_ff @(posedge clk, negedge nrst)
+	begin
+		if(nrst == 0)
+		begin
+			enable_output_counter <= 0;
+			clear_output_counter <= 0;
+		end
+
+		else if((enable_output_counter == 0) && (count_9_16 == 4'd7))
+		begin
+			enable_output_counter <= 1;
+		end
+
+		else if((enable_rounds_9_16 == 0) && (count_output == 4'd7))
+		begin
+			enable_output_counter <= 0;
+			clear_output_counter <= 1;
 		end
 
 
+		else
+		begin
+			enable_output_counter <= enable_output_counter;
+			clear_output_counter <= 0;
+
+		end
 	end
+
+
 
 
 	//KSA
@@ -245,11 +306,6 @@ module DES_block (
 			next_right_reg_9_16 = right_out_round_1_8; 
 		end
 
-		else if(rollover_9_16 == 1)
-		begin
-			output_data_block = {left_out_round_9_16, right_out_round_9_16};
-		end
-
 		else
 		begin
 			next_left_reg_1_8 = left_out_round_1_8;
@@ -257,6 +313,20 @@ module DES_block (
 			next_left_reg_9_16 = left_out_round_9_16;
 			next_right_reg_9_16 = right_out_round_9_16;  
 		end
+	end
+	always_comb
+	begin
+		if(rollover_9_16 == 1)
+		begin
+			output_data_block = {right_out_round_9_16, left_out_round_9_16};
+		end
+
+		else
+		begin
+			output_data_block = output_data_block;
+		end
+
+
 	end
 
 
