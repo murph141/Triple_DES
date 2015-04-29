2cde3fc144d5f60b39a71416c29811aa1adce87b
SethBon <sbontrag@purdue.edu>
2015-04-29 17:02:27 -0400

    fixed latches

--- source/s_box1.sv
+++ source/s_box1.sv
@@ -43,7 +43,7 @@ module s_box1 (
 			endcase
 		end
 
-		if(row == 1)
+		else if(row == 1)
 		begin
 			case(col)
 				0 : temp_out = 0;
@@ -65,7 +65,7 @@ module s_box1 (
 			endcase
 		end
 
-		if(row == 2)
+		else if(row == 2)
 		begin
 			case(col)
 				0 : temp_out = 4;
@@ -87,7 +87,7 @@ module s_box1 (
 			endcase
 		end
 
-		if(row == 3)
+		else if(row == 3)
 		begin
 			case(col)
 				0 : temp_out = 15;
@@ -107,6 +107,11 @@ module s_box1 (
 				14: temp_out = 6;
 				15: temp_out = 13;
 			endcase		
+		end
+
+		else
+		begin
+			temp_out = 0;
 		end
 	end 	
 
