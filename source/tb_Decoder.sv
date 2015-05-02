6f22a3d49350c54fa4fa8dfc28a3a2131c2897e2
murph141 <murph141@purdue.edu>
2015-05-02 12:34:28 -0400

    Decoder is working correctly

--- source/tb_Decoder.sv
+++ source/tb_Decoder.sv
@@ -5,3 +5,34 @@
 // Lab Section: 337-04
 // Version:     1.0  Initial Design Entry
 // Description: Testing the decoder.
+
+module tb_Decoder();
+
+  localparam WAIT = 5;
+
+  logic [31:0] tb_HADDR;
+  logic muxSelect;
+
+  Decoder DUT
+  (
+    .HADDR(tb_HADDR),
+    .muxSelect(muxSelect)
+  );
+
+  initial
+  begin
+    setValues('0);
+    setValues('1);
+    setValues(32'h00001400);
+    setValues(32'h000013FF);
+
+    #WAIT;
+    $finish;
+  end
+
+  task setValues(logic [31:0] addr);
+    #WAIT;
+    tb_HADDR = addr;
+  endtask
+
+endmodule
