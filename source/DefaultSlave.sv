1aa47f51dc931c43de1b7f0b8240607d1470db75
murph141 <murph141@purdue.edu>
2015-05-02 12:20:39 -0400

    Adding new files

---
+++ source/DefaultSlave.sv
@@ -0,0 +1,53 @@
+// $Id: $
+// File name:   DefaultSlave.sv
+// Created:     5/2/2015
+// Author:      Eric Murphy
+// Lab Section: 337-04
+// Version:     1.0  Initial Design Entry
+// Description: Default slave used to take care of requests that originate from addresses that were not assigned to the slave connected to our design.
+//              According to specifications, this slave needs to be present if the system design does not contain a completely filled memory map.
+
+
+module DefaultSlave
+(
+  input logic HCLK,
+  input logic HMASTLOCK,
+  input logic HREADY,
+  input logic HRESET,
+  input logic HSEL,
+  input logic HWRITE,
+  input logic [1:0] HTRANS,
+  input logic [2:0] HBURST,
+  input logic [2:0] HSIZE,
+  input logic [3:0] HPROT,
+  input logic [31:0] HADDR,
+  input logic [63:0] HWDATA,
+
+  output logic HREADYOUT,
+  output logic HRESP,
+  output logic [63:0] HRDATA
+);
+
+  assign HREADYOUT = 1'b1;
+
+  always_ff @ (posedge HCLK, negedge HRESET)
+  begin
+    if(HRESET == 1'b0)
+    begin
+      HRESP <= 1'b0;
+    end
+    else
+    begin
+      if(HRESP == 1'b1)
+        HRESP <= 1'b1;
+      else
+        HRESP <= (HTRANS == 2'b10 || HTRANS == 2'b11);
+    end
+  end
+
+  always_ff @ (posedge HCLK)
+  begin
+  end
+
+
+endmodule
---
+++ source/tb_Decoder.sv
@@ -0,0 +1,7 @@
+// $Id: $
+// File name:   tb_Decoder.sv
+// Created:     5/2/2015
+// Author:      Eric Murphy
+// Lab Section: 337-04
+// Version:     1.0  Initial Design Entry
+// Description: Testing the decoder.
